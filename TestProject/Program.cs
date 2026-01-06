7aace0c8fe82637dc8e3ff13be330997da9e1a1a
Yuta Kobayashi <kobayashi.yuta@firstnurse.co.jp>
2026-01-06 15:16:43 +0900

    4回目:コメント・改行

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -2,75 +2,81 @@
 //Console.WriteLine("Hello C#!");
 
 /*
-string[] fraudulentOrderIDs = new string[3] ;
-
-fraudulentOrderIDs[0] = "A123";
-fraudulentOrderIDs[1] = "B456";
-fraudulentOrderIDs[2] = "C789";
-//fraudulentOrderIDs[3] = "D000";
-*/
-/*
-string[] fraudulentOrderIDs = [ "A123", "B456", "C789" ] ;
-
-Console.WriteLine($"First: {fraudulentOrderIDs[0]}") ;
-Console.WriteLine($"Second: {fraudulentOrderIDs[1]}") ;
-Console.WriteLine($"Third: {fraudulentOrderIDs[2]}") ;
-
-fraudulentOrderIDs[0] = "F000";
-
-Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}") ;
-Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.") ;
+string firstName = "Bob" ;
+int widgetsPurchased = 7 ;
+Console.WriteLine($"{firstName} Purchased {widgetsPurchased} widgets.") ;
 */
 
-//============================================================
-/*
-using System.Collections.Specialized;
-using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Dynamic;
+using System.Net.NetworkInformation;
+using System.Reflection.Emit;
+using System.Security.Cryptography;
 using System.Text;
 
-string[] names = { "Rowena", "Robin", "Bao" } ;
-foreach( String name in names)
-{
-    Console.WriteLine( name ) ;
-}
-*/
-
-//============================================================
 /*
-using System.Linq.Expressions;
-using System.Runtime.CompilerServices;
-
-int[] inventory = { 200, 450, 700, 175, 250 } ;
-int sum = 0 ;
-
-int bin = 0 ;
-
-foreach( int items in inventory )
-{
-    sum += items ;
-    bin++ ;
-    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})") ;
-}
-
-Console.WriteLine($"We have {sum} items in inventory.") ;
+    The following code creates five random OrderIDs
+    to test the fraud detection process. OrderIDs
+    consist of a letter from A to E, and a three
+    digit number. Ex. A123.
 */
+/*
+Random random = new Random() ;
+string[] orderIDs = new string[5] ;
 
-//============================================================
-
-using System.Linq.Expressions;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.Text;
-using System.Xml;
-
-String[] orderID = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" } ;
-int bin = 0 ;
-
-foreach( string name in orderID )
+for( int i = 0 ; i < orderIDs.Length ; i++ )
 {
-    //IDにBを含むか？
-    if( name.StartsWith('B'))
-    {
-        Console.WriteLine($"orderID[{bin}]: {name}") ;
+    int prefixValue = random.Next( 65, 70 ) ;
+    String prefix = Convert.ToChar( prefixValue ).ToString() ;
+    string suffix = random.Next( 1, 1000 ).ToString("000") ;
+
+    orderIDs[i] = prefix + suffix ;
+}
+// Print out each orderID
+foreach( var orderID in orderIDs )
+{
+    Console.WriteLine( orderID ) ;
+}
+*/
+/*
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) {
+    if ((roll1 == roll2) && (roll2 == roll3)) {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6; 
     }
-    bin++ ;
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
 }
+*/
+
+string str = "The quick brown fox jumps over the lazy dog." ;
+
+char[] charMessage = str.ToCharArray() ;
+Array.Reverse( charMessage ) ;
+int x = 0 ;
+
+foreach( char i in charMessage )
+{
+    if( i == 'o')
+    {
+        x++ ;
+    }
+}
+
+string new_message = new String( charMessage ) ;
+
+Console.WriteLine(new_message) ;
+Console.WriteLine($"'o' appears {x} times.") ;
