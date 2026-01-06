ef884b15b72cca3a8bf816886a31e66412768e8d
Yuta Kobayashi <kobayashi.yuta@firstnurse.co.jp>
2026-01-06 13:00:24 +0900

    3回目:foreachステートメント

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -2,94 +2,75 @@
 //Console.WriteLine("Hello C#!");
 
 /*
-using System.Data;
-using System.Diagnostics;
-using System.Numerics;
-using System.Reflection.Emit;
+string[] fraudulentOrderIDs = new string[3] ;
 
-Random dice = new Random() ;
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";
+//fraudulentOrderIDs[3] = "D000";
+*/
+/*
+string[] fraudulentOrderIDs = [ "A123", "B456", "C789" ] ;
 
-int roll1 = dice.Next( 1, 7 ) ;
-int roll2 = dice.Next( 1, 7 ) ;
-int roll3 = dice.Next( 1, 7 ) ;
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}") ;
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}") ;
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}") ;
 
-roll1 = 3;
-roll2 = 2;
-roll3 = 1;
+fraudulentOrderIDs[0] = "F000";
 
-int total = roll1 + roll2 + roll3 ;
+Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}") ;
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.") ;
+*/
 
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}") ;
+//============================================================
+/*
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Text;
 
-if(( roll1 == roll2 ) || ( roll2 == roll3 ) || ( roll1 == roll3 ))
+string[] names = { "Rowena", "Robin", "Bao" } ;
+foreach( String name in names)
 {
-    if(( roll1 == roll2 ) && ( roll2 == roll3 ))
-    {
-        Console.WriteLine("You rolled triples! +6 bonus to total!") ;
-        total += 6 ;
-    }
-    else
-    {
-        Console.WriteLine("You rolled doubles! +2 bonus to total!") ;
-        total += 2 ;
-    }
-}
-
-if( total >= 16 )
-{
-    Console.WriteLine("You win a new car!") ;
-} 
-else if( total >= 10 )
-{
-    Console.WriteLine("You win a new laptop!") ;
-}
-else if( total == 7 )
-{
-    Console.WriteLine("You win a trip for two!") ;
-}
-else
-{
-    Console.WriteLine("You win a kitten!") ;
+    Console.WriteLine( name ) ;
 }
 */
+
 //============================================================
+/*
 using System.Linq.Expressions;
-using System.Security.Cryptography;
+using System.Runtime.CompilerServices;
 
-Random random = new Random() ;
-int dayUntilExpiration = random.Next( 12 ) ;
-int discountPercentage = 0 ;
+int[] inventory = { 200, 450, 700, 175, 250 } ;
+int sum = 0 ;
 
-//有効期限まで10日以下か？
-if( dayUntilExpiration <= 10 )
+int bin = 0 ;
+
+foreach( int items in inventory )
 {
-    //有効期限内か？
-    if ( dayUntilExpiration > 0 )
-    {
-        //有効期限は1日以内か？
-        if( dayUntilExpiration == 1 )
-        {
-            discountPercentage = 20 ;
-            Console.WriteLine("Your subscription expires within a day!") ;
-            Console.WriteLine($"Renew now and save {discountPercentage}%!") ;
-        }
-        //有効期限は5日以内か？
-        else if( dayUntilExpiration <= 5 )
-        {
-            discountPercentage = 10 ;
-            Console.WriteLine($"Your subscription expires in {dayUntilExpiration} days.") ;
-            Console.WriteLine($"Renew now and save {discountPercentage}%!") ;
-        }
-        //有効期限10日以内
-        else
-        {
-            Console.WriteLine("Your subscription will expire soon. Renew now!") ;
-        }
-    }
-    //有効期限切れ
-    else
-    {
-        Console.WriteLine("Your subscription has expired.") ;
-    }
+    sum += items ;
+    bin++ ;
+    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})") ;
 }
 
+Console.WriteLine($"We have {sum} items in inventory.") ;
+*/
+
+//============================================================
+
+using System.Linq.Expressions;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Xml;
+
+String[] orderID = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" } ;
+int bin = 0 ;
+
+foreach( string name in orderID )
+{
+    //IDにBを含むか？
+    if( name.StartsWith('B'))
+    {
+        Console.WriteLine($"orderID[{bin}]: {name}") ;
+    }
+    bin++ ;
+}
