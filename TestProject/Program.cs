6161413d7c710d258ae02ca29f8c1299ce03ffe3
Yuta Kobayashi <kobayashi.yuta@firstnurse.co.jp>
2026-01-07 16:23:00 +0900

    8回目:コードブロック

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,90 +1,51 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello C#!");
-/*
-Console.WriteLine("a" == "a") ;
-Console.WriteLine("a" == "A") ;
-Console.WriteLine( 1 == 2 ) ;
-
-string myValue = "a" ;
-Console.WriteLine( myValue == "a" ) ;
-*/
-
-//string value1 = " a" ;
-//string value2 = "A " ;
-//Console.WriteLine( value1.Trim().ToLower() == value2.Trim().ToLower()) ;
 
 /*
-Console.WriteLine("a" != "a") ;
-Console.WriteLine("a" != "A") ;
-Console.WriteLine( 1 != 2 ) ;
+bool flag = true ;
+int value = 0 ;
 
-string myvalue = "a" ;
-Console.WriteLine( myvalue != "a") ;
-*/
-
-//Console.WriteLine( 1 > 2 ) ;
-//Console.WriteLine( 1 < 2 ) ;
-//Console.WriteLine( 1 >= 1 ) ;
-//Console.WriteLine( 1 <= 1 ) ;
-
-//string pangram = "The quick brown fox jumps over the lazy dog." ;
-// These two lines of code will create the same output
-//Console.WriteLine( pangram.Contains("fox") == false ) ;
-//Console.WriteLine( !pangram.Contains("fox")) ;
-
-//string pangram = "The quick brown fox jumps over the lazy dog." ;
-//Console.WriteLine(!pangram.Contains("fox")) ;
-//Console.WriteLine(!pangram.Contains("cow")) ;
-/*
-int a = 7 ;
-int b = 6 ;
-Console.WriteLine( a != b ) ;// output:a True
-string s1 = "Hello" ;
-string s2 = "Hello" ;
-Console.WriteLine( s1.ToLower() != s2 ) ;// output: False
-*/
-
-//int saleAmount = 1000 ;
-//int saleAmount = 1001 ;
-// int discount = saleAmount > 1000 ? 100 : 50 ;
-//Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}") ;
-
-//演習3-1
-/*
-Random coin = new Random() ;
-int result = coin.Next(1,3) ;
-Console.WriteLine($"result{result}: {(result == 1 ? "heads" : "tails")}") ;
-*/
-
-//string permission = "Admin" ;
-//string permission = "Manager" ;
-string permission = "Guest" ;
-int level = 99 ;
-permission.Contains("Admin") ;
-
-if( permission.Contains("Admin") == true )
+if( flag )
 {
-    if( level > 55 )
-    {
-        Console.WriteLine("Welcome, Super Admin user.") ;
-    }
-    else
-    {
-        Console.WriteLine("Welcome, Admin user.") ;
-    }
+    Console.WriteLine($"Inside the code block: {value}") ;
 }
-else if( permission.Contains("Manager") == true )
-{
-    if( level >= 20 )
-    {
-        Console.WriteLine("Contact an Admin for access.") ;
-    }
-    else
-    {
-        Console.WriteLine("You do not have sufficient privileges.") ;
-    }
-}
+
+value = 10 ;
+Console.WriteLine($"Outside the code block: {value}") ;
+*/
+
+/*
+bool flag = true ;
+if( flag) Console.WriteLine( flag ) ;
+*/
+
+/*
+string name = "steve" ;
+
+if( name == "bob" )
+    Console.WriteLine("Found Bob") ;
+else if( name == "steve" )
+    Console.WriteLine("Found Steve") ;
 else
+    Console.WriteLine("Found chuck") ;
+*/
+
+int[] numbers = { 4, 8, 15, 16, 23, 44 } ;
+int total = 0 ;
+bool found = false ;
+
+foreach(int number in numbers)
 {
-    Console.WriteLine("You do not have sufficient privileges!") ;
+    total += number;
+
+    if( number == 42 )
+       found = true ;
 }
+
+if( found ) 
+    Console.WriteLine("Set contains 42") ;
+else
+
+    Console.WriteLine("Set not contains 42") ;
+
+Console.WriteLine($"Total: {total}") ;
--- TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b03df95f6c9cce9646477d0626545284d2624375")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e851e15f7ea7aaf66cde966681f28ba7c0f56b8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
