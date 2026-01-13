28b2cbc56d5bc8eb78f38eb4a27467e02126ae70
Yuta Kobayashi <kobayashi.yuta@firstnurse.co.jp>
2026-01-13 11:52:31 +0900

    16th: データ型の選択

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -2,183 +2,53 @@
 //Console.WriteLine("Hello C#!");
 
 using System.ComponentModel.DataAnnotations;
-
-Random random = new Random() ;
-int current = random.Next( 1, 11 ) ;
 /*
-do
-{
-    current = random.Next( 1, 11 ) ;
+Console.WriteLine("Signed integral types:");
 
-    if( current >= 8 ) continue ;
+Console.WriteLine($"sbyte   : {sbyte.MinValue} to {sbyte.MaxValue}") ;
+Console.WriteLine($"short   : {short.MinValue} to {short.MaxValue}") ;
+Console.WriteLine($"int     : {int.MinValue} to {int.MaxValue}") ;
+Console.WriteLine($"long    : {long.MinValue} to {long.MaxValue}") ;
 
-    Console.WriteLine( current ) ;
-} while( current != 7 ) ;
-*/
-/*
-while( current >= 3 )
-{
-    Console.WriteLine( current ) ;
-    current = random.Next( 1, 11 ) ;
-}
-Console.WriteLine($"Last number: {current}") ;
-*/
+Console.WriteLine("");
+Console.WriteLine("Unsigned integral types:");
 
-//演習01
-/*
-int heroHealthPoint = 10 ;
-int monsterHealthPoint = 10 ;
-int currentAttackPoint = 0 ;
-Random currentAttackDamege = new Random() ;
-
-while( heroHealthPoint > 0 )
-{
-    currentAttackPoint = currentAttackDamege.Next( 1, 11 ) ;
-    monsterHealthPoint -= currentAttackPoint ;
-    Console.WriteLine($"Monster was damaged and lost {currentAttackPoint} health and now has {monsterHealthPoint} health.") ;
-
-    if( monsterHealthPoint <= 0 )
-    {
-        Console.WriteLine("Hero wins!") ;
-        break ;
-    }
-
-    currentAttackPoint = currentAttackDamege.Next( 1, 11 ) ;
-    heroHealthPoint -= currentAttackPoint ;
-    Console.WriteLine($"Hero was damaged and lost {currentAttackPoint} health and now has {heroHealthPoint} health") ;
-
-    if( heroHealthPoint <= 0 )
-    {
-        Console.WriteLine("Hero is Lose...") ;
-        break ;
-    }
-}
+Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}") ;
+Console.WriteLine($"ushort   : {ushort.MinValue} to {ushort.MaxValue}") ;
+Console.WriteLine($"uint     : {uint.MinValue} to {uint.MaxValue}") ;
+Console.WriteLine($"ulong    : {ulong.MinValue} to {ulong.MaxValue}") ;
 */
 
 /*
-string? readResult ;
-Console.WriteLine("Enter a string:") ;
-do
-{
-    readResult = Console.ReadLine() ;
+Console.WriteLine("");
+Console.WriteLine("Floating point types:");
 
-    int numericValue = 0 ;
-    bool validNumber = false ;
-
-    validNumber = int.TryParse( readResult, out numericValue ) ;
-    Console.WriteLine($"{readResult} {numericValue} {validNumber}") ;
-
-} while( readResult == null ) ;
+Console.WriteLine($"float   : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)") ;
+Console.WriteLine($"double   : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)") ;
+Console.WriteLine($"decimal     : {decimal.MinValue} to {decimal.MaxValue} (with ~28-29 digits of precision)") ;
 */
 
-//演習02-01
 /*
-string? readResult ;
-string resultReadValue = "" ;
-int numericValue = 0 ;
-bool validNumber = false ;
-
-Console.WriteLine("Please enter an integer value between 5 and 10.") ;
-
-do
-{
-    readResult = Console.ReadLine() ;
-    
-    if( readResult != null )
-    {
-        resultReadValue = readResult ;
-        validNumber = int.TryParse( resultReadValue, out numericValue ) ;
-    }
-
-    if(( validNumber == true ) && ( numericValue < 5 ) || ( numericValue > 10 ))
-    {
-        validNumber = false ;
-        Console.WriteLine($"You entered {numericValue}. Please enter a number between 5 and 10.") ;
-    }
-    else if( validNumber == false )
-    {
-        Console.WriteLine("Sorry, you entered an invalid number, please try again") ;
-    }
-
-} while( validNumber == false ) ;
-
-Console.WriteLine($"Your input value ({numericValue}) has been accepted.") ;
-readResult = Console.ReadLine();
+int[] data = new int[3];
+string shortenedstring = "Hello World!" ;
+Console.WriteLine(shortenedstring) ;
 */
 
-//演習02-02
 /*
-string? readResult ;
-string resultReadValue = "" ;
-bool entryResult = false ;
+int val_A = 2;
+int val_B = val_A ;
+val_B = 5;
 
-Console.WriteLine("Enter your role name (Administrator, Manager, or User)") ;
-
-do
-{
-    readResult = Console.ReadLine() ;
-
-    if( readResult != null )
-    {
-        resultReadValue = readResult ;
-        switch( resultReadValue.Trim().ToLower())
-        { 
-            case "administrator":
-            case "manager":
-            case "user":
-                entryResult = true ;
-                break ;
-            default:
-                Console.WriteLine($"The role name that you entered, \"{resultReadValue.Trim()}\" is not valid. Enter your role name (Administrator, Manager, or User)") ;
-                break ;
-        }
-    }
-} while( entryResult == false ) ;
-
-Console.WriteLine($"Your input value ({resultReadValue.Trim()}) has been accepted.") ;
-readResult = Console.ReadLine() ;
-
+Console.WriteLine("--Value Types--");
+Console.WriteLine($"val_A: {val_A}");
+Console.WriteLine($"val_B: {val_B}");
 */
 
-//演習02-03
+int[] ref_A = new int[1];
+ref_A[0] = 2;
+int[] ref_B = ref_A ;
+ref_B[0] = 5;
 
-string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" } ;
-string resultString = "" ;
-int periodLocation = 0 ;
-
-foreach( var myString in myStrings)
-{
-    periodLocation = myString.IndexOf('.') ;
-    resultString = myString ;
-
-    if( periodLocation != -1 )
-    {
-        int stringEndIndex = 0 ;
-
-        do
-        {
-            resultString = resultString.TrimStart() ;
-            periodLocation = resultString.IndexOf('.') ;
-
-            if( periodLocation != -1 )
-            {
-                stringEndIndex = periodLocation + 1 ;
-                Console.WriteLine( $"{resultString.Substring( 0, stringEndIndex - 1 ).TrimStart('.')}" ) ;
-
-            }
-            else
-            {
-                stringEndIndex = resultString.Length ;
-                Console.WriteLine( $"{resultString.Substring( 0, stringEndIndex ).TrimStart('.')}" ) ;
-            }
-
-            resultString = resultString.Remove( 0, stringEndIndex ) ;
-   
-        } while( resultString.Length > 0 ) ;
-    }
-
-    if( resultString.Length > 0)
-    {
-        Console.WriteLine( $"{resultString}" ) ;
-    }
-}
+Console.WriteLine("--Reference Types--");
+Console.WriteLine($"ref_A[0]: {ref_A[0]}");
+Console.WriteLine($"ref_B[0]: {ref_B[0]}");
--- TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9756a6bbf016fcbc4aaa5515c59daacbbc2ccc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fb6616bf18dcf9f9548f3630f34c193b2ac5285")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
