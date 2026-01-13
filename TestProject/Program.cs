6d36ed828fbea5e621e5ca370ab603b1dc7198d4
Yuta Kobayashi <kobayashi.yuta@firstnurse.co.jp>
2026-01-13 16:18:12 +0900

    18th:　ヘルパーメソッドを使用した配列に対する操作

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,129 +1,104 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello C#!");
 
-using System.ComponentModel.DataAnnotations;
-using System.Reflection;
-using System.Windows.Markup;
 /*
-int first = 2;
-string second = "4";
-string result = first + second;
-Console.WriteLine(result);
-*/
+string[] pallets = ["B14", "A11", "B12", "A13"];
 
-/*
-int myInt = 3 ;
-Console.WriteLine($"int: {myInt}");
-
-decimal myDecimal = myInt;
-Console.WriteLine($"decimal: {myDecimal}");
-*/
-
-/*
-decimal myDecimal = 3.14m;
-Console.WriteLine($"decimal: {myDecimal}");
-
-int myInt = (int)myDecimal;
-Console.WriteLine($"int: {myInt}");
-*/
-
-/*
-decimal myDecimal = 1.23456789m;
-float myFloat = (float)myDecimal;
-
-Console.WriteLine($"Decimal : {myDecimal}");
-Console.WriteLine($"Float   : {myFloat}");
-*/
-
-/*
-int first = 5;
-int second = 7;
-string message = first.ToString() + second.ToString();
-Console.WriteLine(message);
-*/
-
-/*
-string first = "5";
-string second = "7";
-int sum = int.Parse(first) + int.Parse(second);
-Console.WriteLine(sum);
-*/
-
-/*
-string value1 = "5";
-string value2 = "7";
-int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
-Console.WriteLine(result);
-*/
-
-/*
-int value = (int)1.5m; // casting truncates
-Console.WriteLine(value);
-
-int value2 = Convert.ToInt32(1.5m); // converting rounds up
-Console.WriteLine(value2);
-*/
-
-/*
-string name = "Bob";
-Console.WriteLine(int.Parse(name));
-*/
-
-/*
-string value = "bad";
-int result = 0;
-if (int.TryParse(value, out result))
+Console.WriteLine("Sorted...");
+Array.Sort(pallets);
+foreach (var pallet in pallets)
 {
-    Console.WriteLine($"Measurement: {result}");
-}
-else
-{
-    Console.WriteLine("Unable to report the measurement.");
+    Console.WriteLine($"-- {pallet}");
 }
 
-if (result > 0)
-    Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+Console.WriteLine("");
+Console.WriteLine("Reversed...");
+Array.Sort(pallets);
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
 */
 
 /*
-string[] ValueSerializerAttribute = { "12.3", "45", "ABC", "11", "DEF" };
-string alphabet = "";
-decimal total = 0m;
-decimal outDecimal = 0m;
+string[] pallets = ["B14", "A11", "B12", "A13"];
+Console.WriteLine("");
 
-foreach (string value in ValueSerializerAttribute)
+Array.Clear(pallets, 0, 2);
+Console.WriteLine($"Cleaning 2 ... count: {pallets.Length}");
+foreach (var pallet in pallets)
 {
-    if (decimal.TryParse(value, out outDecimal))
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Array.Resize(ref pallets, 6);
+Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+pallets[4] = "C01";
+pallets[5] = "C02";
+
+foreach(var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Array.Resize(ref pallets, 3);
+Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+
+foreach(var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+*/
+
+/*
+string value = "abc123";
+char[] valueArray = value.ToCharArray();
+Array.Reverse(valueArray);
+// string result = new string(valueArray);
+string result = string.Join(",", valueArray);
+Console.WriteLine(result);
+
+string[] items = result.Split(',');
+foreach (string item in items)
+{
+    Console.WriteLine(item);
+}
+*/
+
+/*
+string pangram = "The quick brown fox jumps over the lazy dog";
+string pangramReverse = "";
+string[] results = pangram.Split(' ');
+
+for (int i = 0; i < results.Length; i++)
+{
+    char[] word = results[i].ToCharArray();
+    Array.Reverse(word);
+    string reverseWord = new string(word);
+    results[i] = reverseWord ;
+}
+
+pangramReverse = string.Join(" ", results);
+
+Console.WriteLine($"{pangramReverse}");
+*/
+
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] orderIDs = orderStream.Split(',');
+
+Array.Sort(orderIDs);
+
+foreach( var orderID in orderIDs)
+{
+    if(orderID.Length == 4)
     {
-        total += outDecimal;
+        Console.WriteLine($"{orderID}");
     }
     else
     {
-        alphabet = alphabet + value ;
+        Console.WriteLine($"{orderID}\t- Error");
     }
-
 }
-Console.WriteLine($"Message: {alphabet}");
-Console.WriteLine($"Total: {total}");
-*/
-
-int value1 = 11;
-decimal value2 = 6.2m;
-float value3 = 4.3f;
-
-decimal result1 = 0m;
-decimal result2 = 0m;
-float result3 = 0f;
-
-// Your code here to set result1
-// Hint: You need to round the result to nearest integer (don't just truncate)
-result1 = (decimal)value1 / value2;
-Console.WriteLine($"Divide value1 by value2, display the result as an int: {Convert.ToInt32(result1)}");
-
-// Your code here to set result2
-result2 = value2 / (decimal)value3;
-Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
-
-// Your code here to set result3
-result3 = value3 / (float)(value1) ;
-Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
--- TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28b2cbc56d5bc8eb78f38eb4a27467e02126ae70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+537e5b7829447a2b777b14bb40dc5f1695900655")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
