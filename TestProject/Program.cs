12ad0893519d05ef249204f0fd659a8d532346a1
Yuta Kobayashi <kobayashi.yuta@firstnurse.co.jp>
2026-01-14 09:19:24 +0900

    19th: 英数字データの書式設定

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -2,103 +2,101 @@
 //Console.WriteLine("Hello C#!");
 
 /*
-string[] pallets = ["B14", "A11", "B12", "A13"];
-
-Console.WriteLine("Sorted...");
-Array.Sort(pallets);
-foreach (var pallet in pallets)
-{
-    Console.WriteLine($"-- {pallet}");
-}
-
-Console.WriteLine("");
-Console.WriteLine("Reversed...");
-Array.Sort(pallets);
-foreach (var pallet in pallets)
-{
-    Console.WriteLine($"-- {pallet}");
-}
+string first = "Hello";
+string second = "World";
+Console.WriteLine($"{first} {second}!");
+Console.WriteLine($"{second} {first}!");
+Console.WriteLine($"{first} {first} {first}!");
 */
 
 /*
-string[] pallets = ["B14", "A11", "B12", "A13"];
-Console.WriteLine("");
-
-Array.Clear(pallets, 0, 2);
-Console.WriteLine($"Cleaning 2 ... count: {pallets.Length}");
-foreach (var pallet in pallets)
-{
-    Console.WriteLine($"-- {pallet}");
-}
-
-Console.WriteLine("");
-Array.Resize(ref pallets, 6);
-Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
-
-pallets[4] = "C01";
-pallets[5] = "C02";
-
-foreach(var pallet in pallets)
-{
-    Console.WriteLine($"-- {pallet}");
-}
-
-Console.WriteLine("");
-Array.Resize(ref pallets, 3);
-Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
-
-foreach(var pallet in pallets)
-{
-    Console.WriteLine($"-- {pallet}");
-}
+decimal price = 123.45m;
+int discount = 50;
+Console.WriteLine($"Price: {price:C} (Save {discount:C})");
 */
 
 /*
-string value = "abc123";
-char[] valueArray = value.ToCharArray();
-Array.Reverse(valueArray);
-// string result = new string(valueArray);
-string result = string.Join(",", valueArray);
-Console.WriteLine(result);
-
-string[] items = result.Split(',');
-foreach (string item in items)
-{
-    Console.WriteLine(item);
-}
+decimal measurement = 123456.78912m;
+Console.WriteLine($"Measurement: {measurement:N4} units") ;
 */
 
 /*
-string pangram = "The quick brown fox jumps over the lazy dog";
-string pangramReverse = "";
-string[] results = pangram.Split(' ');
-
-for (int i = 0; i < results.Length; i++)
-{
-    char[] word = results[i].ToCharArray();
-    Array.Reverse(word);
-    string reverseWord = new string(word);
-    results[i] = reverseWord ;
-}
-
-pangramReverse = string.Join(" ", results);
-
-Console.WriteLine($"{pangramReverse}");
+decimal tax = .36785m;
+Console.WriteLine($"Tax rate: {tax:P2}");
 */
 
-string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
-string[] orderIDs = orderStream.Split(',');
+/*
+decimal price = 67.55m;
+decimal salePrice = 59.99m;
 
-Array.Sort(orderIDs);
+string yourDiscount = string.Format("You saved {0:C2} off the regular {1:C2} price.", (price - salePrice), price);
 
-foreach( var orderID in orderIDs)
-{
-    if(orderID.Length == 4)
-    {
-        Console.WriteLine($"{orderID}");
-    }
-    else
-    {
-        Console.WriteLine($"{orderID}\t- Error");
-    }
-}
+yourDiscount += $"A discount of {((price - salePrice)/price):P2}!"; //inserted
+Console.WriteLine(yourDiscount);
+*/
+
+/*
+int invoiceNumber = 1201;
+decimal productShares = 25.4568m;
+decimal subtotal = 2750.00m;
+decimal taxPercentage = .15825m;
+decimal total = 3185.19m;
+
+Console.WriteLine($"Invoice number: {invoiceNumber}");
+Console.WriteLine($"   shares: {productShares:N3} Product");
+Console.WriteLine($"     sub Total: {subtotal:C}");
+Console.WriteLine($"           Tax: {taxPercentage:P2}");
+Console.WriteLine($"     Total Billed: {total:C}");
+*/
+
+/*
+string input = "Pad this";
+Console.WriteLine(input.PadRight(12));
+*/
+
+/*
+string input = "Pad this";
+Console.WriteLine(input.PadLeft(12, '-'));
+Console.WriteLine(input.PadRight(12, '-'));
+*/
+
+/*
+string paymentId = "769C";
+string payeeName = "Mr. Stephen Ortega";
+string paymentAmount = "$5,000.00";
+
+var formattedLine = paymentId.PadRight(6);
+formattedLine += payeeName.PadRight(24);
+formattedLine += paymentAmount.PadLeft(10);
+
+Console.WriteLine("1234567890123456789012345678901234567890");
+Console.WriteLine(formattedLine);
+*/
+
+string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+// Your logic here
+Console.WriteLine($"Dear {customerName},");
+Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+Console.WriteLine($"Currently, you own {currentShares:N2} shares at a return of {currentReturn:P2}.\n");
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P2}.  Given your current volume, your potential profit would be {newProfit:C2}.\n");
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = "";
+
+// Your logic here
+comparisonMessage += string.Format("{0}{1:P2}", currentProduct.PadRight(20), currentReturn);
+comparisonMessage += string.Format("{0:C2}\n", currentProfit).PadLeft(18);
+comparisonMessage += string.Format("{0}{1:P2}", newProduct.PadRight(20), newReturn);
+comparisonMessage += string.Format("{0:C2}\n", newProfit).PadLeft(18);
+
+Console.WriteLine(comparisonMessage);
--- TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+537e5b7829447a2b777b14bb40dc5f1695900655")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0d96e17f71c55647ebc230d6cb6f57ae57915a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
