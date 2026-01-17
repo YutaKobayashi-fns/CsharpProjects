0c4fd4a84efc90fbf862d66e2ea337631f265e18
Yuta Kobayashi <kobayashi.yuta@firstnurse.co.jp>
2026-01-17 09:57:45 +0900

    追加:ファイル整理のため、ChallengeProject-03を再度追加

--- GuidedProject-03/Starter/obj/Debug/net10.0/Starter.AssemblyInfo.cs
+++ GuidedProject-03/Starter/obj/Debug/net10.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afcb9295681f13fa628d4f91cf7d071b2691bd38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c0d642afc3cf44d290df219b2dd578bb0216089")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
